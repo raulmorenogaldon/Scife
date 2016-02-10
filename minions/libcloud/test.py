7a3b6111c3e78881706c33697121a9ea48cf81e0
Raul Moreno <raulmorenogaldon@gmail.com>
2016-02-10 11:44:22 +0100

    Added some error checking in test.py

--- minions/libcloud/test.py
+++ minions/libcloud/test.py
@@ -4,41 +4,47 @@ from os import environ as env
 # Testing
 print "======== OpenStack ========="
 openstack = LibCloudMinion()
-config = {
-    'url': env['OS_AUTH_URL'],
-    'username': env['OS_USERNAME'],
-    'password': env['OS_PASSWORD'],
-    'project': env['OS_TENANT_NAME'],
-    'region': env['OS_REGION_NAME'],
-    'provider': "OpenStack"
-}
-openstack.login(config)
+try:
+    config = {
+        'url': env['OS_AUTH_URL'],
+        'username': env['OS_USERNAME'],
+        'password': env['OS_PASSWORD'],
+        'project': env['OS_TENANT_NAME'],
+        'region': env['OS_REGION_NAME'],
+        'provider': "OpenStack"
+    }
+    openstack.login(config)
 
-images = openstack.getImages()
-for image in images:
-    print image
+    images = openstack.getImages()
+    for image in images:
+        print image
 
-flavors = openstack.getFlavors()
-for flavor in flavors:
-    print flavor
+    flavors = openstack.getFlavors()
+    for flavor in flavors:
+        print flavor
+except Exception as e:
+    print "Error in OpenStack test: ", e
 
 # Testing
 print "======== OpenNebula ========="
-opennebula = LibCloudMinion()
-config = {
-    'url': "http://vesuvius.i3a.uclm.es",
-    'port': 2474,
-    'username': "oneadmin",
-    'password': "35a16a51404255ba4a4b0b66ea9ec5d4",
-    'provider': "OpenNebula"
-}
-opennebula.login(config)
+try:
+    opennebula = LibCloudMinion()
+    config = {
+        'url': "http://vesuvius.i3a.uclm.es",
+        'port': 2474,
+        'username': "oneadmin",
+        'password': "35a16a51404255ba4a4b0b66ea9ec5d4",
+        'provider': "OpenNebula"
+    }
+    opennebula.login(config)
 
-flavors = opennebula.getFlavors()
-for flavor in flavors:
-    print flavor
+    flavors = opennebula.getFlavors()
+    for flavor in flavors:
+        print flavor
 
-images = opennebula.getImages()
-for image in images:
-    print image
+    images = opennebula.getImages()
+    for image in images:
+        print image
+except Exception as e:
+    print "Error in OpenNebula test: ", e
 
