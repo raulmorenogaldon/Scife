e06536fb2db5ec965cad273bd84250a1468fbaef
Raul Moreno <raulmorenogaldon@gmail.com>
2016-02-12 14:00:42 +0100

    Added function description comments

--- minions/cluster/cl_minion.py
+++ minions/cluster/cl_minion.py
@@ -1,15 +1,19 @@
-import zerorpc
-import uuid
+"""Minion for clusters."""
 import json
+import uuid
+import zerorpc
 
+from minions import minion
 from pexpect import pxssh
-from .. import minion
 
 from urlparse import urlparse
 
+
 class ClusterMinion(minion.Minion):
+    """Class definition."""
 
     def __init__(self):
+        """Init function."""
         self.connected = False
 
         # Initialize list of images
@@ -18,10 +22,10 @@ class ClusterMinion(minion.Minion):
         # Initialize list of flavors
         self.flavors = []
 
-    # Login to cluster (SSH)
     def login(self, config):
+        """Login to cluster using SSH."""
         if self.connected:
-            print "Already connected..."
+            print("Already connected...")
             return 0
 
         # Connect to provider
@@ -35,21 +39,21 @@ class ClusterMinion(minion.Minion):
         url = urlparse(config['url'])
         url = username + "@" + url.path
         try:
-            print "Connecting..."
-            print "User: " + username
-            print "Endpoint: " + url
+            print("Connecting...")
+            print("User: {0}".format(username))
+            print("Endpoint: {0}".format(url))
 
             # Create command line and connect to the cluster
             ssh = pxssh.pxssh(echo=False)
             ssh.login(url, username, password)
             ssh.prompt()
-            print "Connected!"
+            print("Connected!")
 
             # Save connection var
             self.ssh = ssh
 
         except Exception as e:
-            print 'FAILED to connect to ' + url + ". Reason: ", e
+            print("FAILED to connect to", url, "- Reason:", e)
             raise e
 
         # Get configuration from cloud.json
@@ -59,24 +63,33 @@ class ClusterMinion(minion.Minion):
             config = json.loads(ssh.before)
             self.__loadConfig(config)
         except Exception as e:
-            print "Malformed config.json!"
+            print("Malformed config.json!")
             raise e
 
         return
 
     def getImages(self, filter=""):
+        """Get image list using an optional name filter."""
         return self.images
 
     def getFlavors(self, filter=""):
+        """Get flavor list using an optional name filter."""
         return self.flavors
 
     def createFlavor(self, flavor):
+        """Create a new flavor and assign an UUID."""
         flavor['id'] = str(uuid.uuid1())
         self.flavors.append(flavor)
-        print "Created flavor:"
-        print flavor
+        print("Created flavor:")
+        print(flavor)
 
+    def deployApplication(self, application):
+        """Deploys an application in the cluster FS."""
+        print("Deploying: ", application['name'])
+
+    """ Private functions """
     def __loadConfig(self, config):
+        print("Loading config...")
         # Parse images
         for image in config['images']:
             image['id'] = str(uuid.uuid1())
@@ -86,6 +99,8 @@ class ClusterMinion(minion.Minion):
         for flavor in config['flavors']:
             flavor['id'] = str(uuid.uuid1())
             self.flavors.append(flavor)
+
+        print("Config loaded!")
 
 
 # Start RPC minion
