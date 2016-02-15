36dc5b7397e14b80c38ba9cafc3e2db2748ec95d
fcoperezgil <franciscovalde@hotmail.com>
2016-02-15 16:53:24 +0100

    Changeg nodejs app to allow json request in an example

--- overlord/express-app/routes/index.js
+++ overlord/express-app/routes/index.js
@@ -3,7 +3,12 @@ var router= express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index/index');
+	if(req.get('accept').indexOf('json') > -1){
+		res.setHeader('Content-Type', 'application/json');
+		res.send(JSON.stringify({return: 'Ha solicitado un objeto json'}));
+	}else{
+		res.render('index/index');
+	}
 });
 
 module.exports = router;
