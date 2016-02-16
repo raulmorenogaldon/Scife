972674a5129c5ec746a291d1b3bb57c248145faf
fcoperezgil <franciscovalde@hotmail.com>
2016-02-16 10:02:41 +0100

    Create an auxiliar function that returns the type of responds that the browser has resquested

--- overlord/express-app/bin/aux.js
+++ overlord/express-app/bin/aux.js
@@ -1,11 +1,27 @@
-function getOrderedAccept(req){
-	var list = {};
-	
+/**
+ *This module implements auxiliar functions
+ */
+module.exports = {
+    /**
+     * Return the string "html" or "json" that appearing firs in the string passed as parameter. If you pass a string with don't contain "html" neither "json" words, the function returns null.
+     */
+    getOrderedAccept: function(req) {
 
-	list['html'] = req.indexOf('html') > -1 ? req.indexOf('html') : null;
-	list['json'] = req.indexOf('json') > -1 ? req.indexOf('json') : null;
-	list['xml'] = req.indexOf('xml') > -1 ? req.indexOf('xml') : null;
+        var html = req.indexOf('html') > -1 ? req.indexOf('html') : null;
+        var json = req.indexOf('json') > -1 ? req.indexOf('json') : null;
 
+        if (html != null && json != null) {
+            if (html < json) {
+                return 'html';
+            } else {
+                return 'json';
+            }
+        } else if (html != null) {
+            return 'html';
+        } else if (json != null) {
+            return 'json'
+        }
+
+        return null;
+    }
 }
-
-getOrderedAccept("ñlaskjfñlasdkjhtml");
--- overlord/express-app/routes/index.js
+++ overlord/express-app/routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router= express.Router();
+var aux =  require('../bin/aux')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	if(req.get('accept').indexOf('json') > -1){
+	if(aux.getOrderedAccept(req.get('accept')) == 'json'){
 		res.setHeader('Content-Type', 'application/json');
 		res.send(JSON.stringify({return: 'Ha solicitado un objeto json'}));
-	}else{
+	}else if(aux.getOrderedAccept(req.get('accept')) == 'html'){
 		res.render('index/index');
+	}else{
+		res.status(404).send({error: 'You need specify what type of respond you want (html or json)'});
 	}
 });
 
