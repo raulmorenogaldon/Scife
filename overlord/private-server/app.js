1a600708eba438b36618fb1ee3c5f63cc54ac9e4
fcoperezgil <franciscovalde@hotmail.com>
2016-02-16 14:45:14 +0100

    Organized the overlord folder, I have created two servers, one which will implement the private server and the other will implement the public server. These two servers can communicate by API REST

--- overlord/private-server/app.js
+++ overlord/private-server/app.js
@@ -9,10 +9,6 @@ var routerStack = require('./routes/openstack');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
---
+++ overlord/private-server/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ return: 'Esto lo devuelve el servidor interno' }));
+});
+
+module.exports = router;
+
