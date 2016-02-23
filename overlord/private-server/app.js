58cc780a52bb0833c261ec514a3ea2539c8b108f
fcoperezgil <franciscovalde@hotmail.com>
2016-02-23 15:11:10 +0100

    Created basic REST request to get info fron the minion server

--- overlord/private-server/app.js
+++ overlord/private-server/app.js
@@ -1,55 +1,54 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+  morgan = require('morgan'),
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser')
 
-var routerIndex = require('./routes/index');
-var routerNebula = require('./routes/opennebula');
-var routerStack = require('./routes/openstack');
+var routerIndex = require('./routes/index'),
+  routerCloud = require('./routes/cloud')
 
-var app = express();
+var app = express()
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routerIndex);
-app.use('/opennebula', routerNebula);
-app.use('/openstack', routerStack);
+app.use('/', routerIndex)
+app.use('/cloud', routerCloud)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+  app.use(morgan('dev'));
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-
-module.exports = app;
+module.exports = app
--- overlord/private-server/routes/index.js
+++ overlord/private-server/routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ return: 'Esto lo devuelve el servidor interno' }));
+    //res.setHeader('Content-Type', 'application/json');
+    //res.send(JSON.stringify({ return: 'Esto lo devuelve el servidor interno' }));
+	 res.json({ return: 'Esto lo devuelve el servidor interno' });
 });
 
 router.get('/suma', function(req, res, next) {
