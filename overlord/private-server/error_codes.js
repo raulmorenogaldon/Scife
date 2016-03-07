f9e75c29285fa15f1ba5e7c71f5d9fa9b77f13fd
Raul Moreno <raulmorenogaldon@gmail.com>
2016-03-07 16:51:48 +0100

    [OVERLORD] Added error codes file. Adapted experiment paths to match new URLs.

---
+++ overlord/private-server/error_codes.js
@@ -0,0 +1,40 @@
+var HTTPCODE = {
+   OK: 200,
+   BAD_REQUEST: 400,
+   NOT_FOUND: 404,
+   INTERNAL_ERROR: 500,
+   NOT_IMPLEMENTED: 501
+}
+
+var ERRCODE = {
+   // General error
+   UNKNOWN: {
+      'message': "Internal error",
+      'code': 1
+   },
+
+   // Development
+   NOT_IMPLEMENTED: {
+      'message': "Not Implemented",
+      'code': 10
+   },
+
+   // ID errors
+   ID_NOT_FOUND: {
+      'message': "Requested ID has not been found!",
+      'code': 20
+   },
+
+   // Experiment
+   EXP_NOT_FOUND: {
+      'message': "Requested experiments has not been found!",
+      'code': 30
+   },
+   EXP_INCORRECT_PARAMS: {
+      'message': "You must pass 'name' and 'app_id'.",
+      'code': 31
+   },
+}
+
+exports.HTTPCODE = HTTPCODE;
+exports.ERRCODE = ERRCODE;
