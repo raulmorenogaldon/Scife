af2c63a6e27d80915fbf3e178509f2d96fdacd0c
Raul Moreno <raulmorenogaldon@gmail.com>
2016-11-30 16:55:14 +0100

    [OVERLORD] Execution list is now ordered by creation date.

--- overlord/execution.js
+++ overlord/execution.js
@@ -193,7 +193,7 @@ var searchExecutions = function(fields, cb){
    };
 
    // Retrieve experiment metadata
-   database.db.collection('executions').find(query, projection).toArray(function(error, execs){
+   database.db.collection('executions').find(query, projection).sort({create_date: -1}).toArray(function(error, execs){
       if(error) return cb(new Error("Query for executions failed"));
       cb(null, execs);
    });
