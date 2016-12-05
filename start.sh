c370a2b89d6d252332e114ff415ef790caf5b0e3
Raul Moreno <raulmorenogaldon@gmail.com>
2016-12-05 10:05:33 +0100

    [OVERLORD] Fixed start script.

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "#############################################"
-
 # Parameters
 NODE=v5.5.0
 BRANCH=develop
@@ -13,22 +11,29 @@ OVERLORD="overlord/app.js ./overlord.cfg"
 UUID_STORAGE="storage"
 STORAGE="storage/storage.js ./storage.cfg"
 
+echo "#############################################"
+
+# Sourcing
+. ~/.nvm/nvm.sh 2> /dev/null
+. ~/.profile 2> /dev/null
+. ~/.bashrc 2> /dev/null
+
 # Update repository
 echo "Updating repository..."
-git pull origin $BRANCH >/dev/null 2>&1
+git pull origin $BRANCH > /dev/null 2>&1
 if [ $? -ne 0 ]; then
 	echo "Repository update failed!"
 fi
 
 # Load NodeJS
 echo "Loading NodeJS..."
-hash node
+hash node > /dev/null 2>&1
 if [ $? -ne 0 ]; then
 	nvm use $NODE
 fi
 
 # Check if loaded correctly
-hash node
+hash node > /dev/null 2>&1
 if [ $? -ne 0 ]; then
 	echo "NodeJS not found!"
 	exit 1
@@ -36,7 +41,8 @@ fi
 
 # Update packages
 echo "Installing NodeJS packages..."
-npm install >/dev/null 2>&1
+npm install -g forever > /dev/null 2>&1
+npm install > /dev/null 2>&1
 if [ $? -ne 0 ]; then
 	echo "Package updating failed!"
 fi
